c282abcf451fab8806fa112817465ca87355d4af
leo8117 <8117leo@gmail.com>
2026-03-03 13:40:35 -0500

    add rental types

--- web/services/inventory/rentals/types.ts
+++ web/services/inventory/rentals/types.ts
@@ -1,11 +1,12 @@
 export interface Rental {
     name: string,
     id: number,
-    location?: string,
-    quantity?: number,
-    status?: status,
-    equipmentType: equipmentType
+    location: string,
+    quantity: number,
+    status: status,
+    equipmentType: equipmentType,
+    notes: string;
 }
 
-type status = "rented" | "inStock" | "outStock"
+type status = "onLoan" | "returned" | "overdue"
 type equipmentType = "electronic" | "stationary" | "lab" | "misc"
