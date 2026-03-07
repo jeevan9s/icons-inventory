55da5fc5249be0682a70fe197f77196e43afe828
Spencer Tapp <spentapp@outlook.com>
2026-03-07 18:21:23 -0500

    Completed the part of the Data function that gets ALL data.

--- web/app/databasetesting/page.tsx
+++ web/app/databasetesting/page.tsx
@@ -6,7 +6,7 @@ export default function DatabaseTestingPage() {
     return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <h1>Database Testing Page</h1>
-        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => getData("Testing Table")}>Get All Data</button>
+        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ async () => console.log( await getData("Testing Table"))}>Get All Data</button>
         <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => addEntry(55)}>Add entry</button>
         <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => deleteEntry(55)}>Delete entry</button>
     </div>
--- web/services/lib/database-functions/databaseHelpers.ts
+++ web/services/lib/database-functions/databaseHelpers.ts
@@ -1,6 +1,7 @@
 "use client"; // must be a Client Component to use browser APIs
+import { ValueOf } from 'next/dist/shared/lib/constants';
+import { Database } from './database.types';
 import { createClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUB_KEY
@@ -11,16 +12,24 @@ const supabase = createClient<Database>(supabaseUrl!, supabaseKey!)
 //  First optional parameter specifies the end entry, second specifies the count of items.
 //  Second parameter specifies the starting entry.
 //  If only count is provided, it will return the 'count' most recent entries
-export async function getData(table: string, count?: number, begin?: number) {
-    console.log(`Getting entries ${begin} to ${count} from ${table} ...`); // TODO remove this when implemented
-    let entries: Array<Object>;
-    if (count === undefined && begin === undefined) { 
-        let data =
-        while    
+//
+//  For full functionality, ensure that database.types.ts is up to date.
+export async function getData<table_name extends keyof Database['public']['Tables']>(table: table_name, count?: number, begin?: number) {
+    console.log(`Getting entries ${begin} to ${count} from ${table} ...`); // TODO remove this when testing completed
+    let entries: Array<Object> = [];
+    if (count === undefined && begin === undefined) { // if nothing specified, get all entries
+        const { count } = await supabase.from(table).select("", { count: 'exact'});
+        if (count == null) return; 
+        for (let i = count; i >= 0; i-=1000) {
+            let { data, error } = await supabase
+            .from(table)
+            .select("*")
+            .range(Math.max(0, i-1000), i);
+            if (data == null) break;
+            entries = entries.concat(data);
+        }
+        return entries;
     }
-    let { data, error } = await supabase
-    .from(table as "Loan Items" | "Loans" | "Stock" | "Testing Table" | "Profiles")
-    .select('*');
 }
 
 export async function addEntry(val: number) {
