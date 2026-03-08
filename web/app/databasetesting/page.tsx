49ef6ab53e901c8bdc00662c1dbd98d6d7d787de
Spencer Tapp <spentapp@outlook.com>
2026-03-08 11:23:35 -0400

    Completed, all of the get data function.

--- web/app/databasetesting/page.tsx
+++ web/app/databasetesting/page.tsx
@@ -6,8 +6,8 @@ export default function DatabaseTestingPage() {
     return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <h1>Database Testing Page</h1>
-        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ async () => console.log( await getData("Testing Table"))}>Get All Data</button>
-        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => addEntry(55)}>Add entry</button>
+        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ async () => console.log( await getData("Testing Table", "created_at", true, 6, 2))}>Get All Data</button>
+        <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => addEntry(Math.round(Math.random()*100))}>Add entry</button>
         <button className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={ () => deleteEntry(55)}>Delete entry</button>
     </div>
     );
--- web/services/lib/database-functions/databaseHelpers.ts
+++ web/services/lib/database-functions/databaseHelpers.ts
@@ -1,5 +1,4 @@
 "use client"; // must be a Client Component to use browser APIs
-import { ValueOf } from 'next/dist/shared/lib/constants';
 import { Database } from './database.types';
 import { createClient } from '@supabase/supabase-js'
 
@@ -7,25 +6,75 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUB_KEY
 const supabase = createClient<Database>(supabaseUrl!, supabaseKey!)
 
-//  Gets data entries from specified table
+//  Gets data entries from specified table ordered based on an input column name
 //  When no additional parameters supplied, returns all entries in table.
-//  First optional parameter specifies the end entry, second specifies the count of items.
-//  Second parameter specifies the starting entry.
-//  If only count is provided, it will return the 'count' most recent entries
+//  First optional parameter specifies the number of entries to return, or if -1, explicitly return all elements.
+//  Second optional parameter specifies the starting entry (inclusively indexed at zero).
 //
 //  For full functionality, ensure that database.types.ts is up to date.
-export async function getData<table_name extends keyof Database['public']['Tables']>(table: table_name, count?: number, begin?: number) {
+export async function getData<table_name extends keyof Database['public']['Tables'],column extends keyof Database['public']['Tables'][table_name]['Row']>(table: table_name, order: column, ascending: boolean, count?: number, begin?: number) {
+    let ordString: string = order as string;
     console.log(`Getting entries ${begin} to ${count} from ${table} ...`); // TODO remove this when testing completed
     let entries: Array<Object> = [];
-    if (count === undefined && begin === undefined) { // if nothing specified, get all entries
+    // get all entries
+    if ((count === undefined || count === -1) && begin === undefined) {
         const { count } = await supabase.from(table).select("", { count: 'exact'});
         if (count == null) return; 
         for (let i = count; i >= 0; i-=1000) {
             let { data, error } = await supabase
             .from(table)
             .select("*")
+            .order(ordString, { ascending: ascending })
             .range(Math.max(0, i-1000), i);
             if (data == null) break;
+            if (error) console.error(error);
+            entries = entries.concat(data);
+        }
+        return entries;
+    }
+    // get count entries
+    else if (begin === undefined && (count !== undefined && count !== -1)) {
+        count --;
+        for (let i = count; i >= 0; i-=1000) {
+            let { data, error } = await supabase
+            .from(table)
+            .select("*")
+            .order(ordString, { ascending: ascending })
+            .range(Math.max(0, i-1000), i);
+            if (data == null) break;
+            if (error) console.error(error);
+            entries = entries.concat(data);
+        }
+        return entries;
+    }
+    // get count entries, starting at begin
+    else if (begin !== undefined && (count !== undefined && count !== -1)) {
+        count --;
+        for (let i = count+begin; i >= begin; i-=1000) {
+            let { data, error } = await supabase
+            .from(table)
+            .select("*")
+            .order(ordString, { ascending: ascending })
+            .range(Math.max(begin, i-1000), i);
+            if (data == null) break;
+            if (error) console.error(error);
+            entries = entries.concat(data);
+        }
+        return entries;
+    }
+    // get all entries starting at begin
+    else {
+        const { count } = await supabase.from(table).select("", { count: 'exact'});
+        if (count == null) return; 
+        console.log("count:", count)
+        for (let i = begin!; i <= count-begin!; i+=1000) {
+            let { data, error } = await supabase
+            .from(table)
+            .select("*")
+            .order(ordString, { ascending: ascending })
+            .range(i, count);
+            if (data == null) break;
+            if (error) console.error(error);
             entries = entries.concat(data);
         }
         return entries;
@@ -38,10 +87,10 @@ export async function addEntry(val: number) {
     .from('Testing Table')
     .insert({value: val})
     .select()
+    if (error) console.error(error);
     data?.forEach((row) => {
         console.log("Row ", row);
     });
-    console.log("Error: ",error);
 }
 
 export async function deleteEntry(val: number) {
